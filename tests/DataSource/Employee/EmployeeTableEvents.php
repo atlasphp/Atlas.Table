605e4e0ff01d4d08919771e1d26dcef251f1a502
Paul M. Jones <pmjones88@gmail.com>
2021-06-11 14:43:21 -0500

    major reformation of generated class structure

--- tests/DataSource/Employee/EmployeeTableEvents.php
+++ tests/DataSource/Employee/EmployeeTableEvents.php
@@ -3,15 +3,6 @@ declare(strict_types=1);
 
 namespace Atlas\Table\DataSource\Employee;
 
-use Atlas\Query\Delete;
-use Atlas\Query\Insert;
-use Atlas\Query\Select;
-use Atlas\Query\Update;
-use Atlas\Table\Row;
-use Atlas\Table\Table;
-use Atlas\Table\TableEvents;
-use PDOStatement;
-
-class EmployeeTableEvents extends TableEvents
+class EmployeeTableEvents extends _\EmployeeTableEvents_
 {
 }
--- tests/DataSource/Nopkey/NopkeyTableEvents.php
+++ tests/DataSource/Nopkey/NopkeyTableEvents.php
@@ -3,15 +3,6 @@ declare(strict_types=1);
 
 namespace Atlas\Table\DataSource\Nopkey;
 
-use Atlas\Query\Delete;
-use Atlas\Query\Insert;
-use Atlas\Query\Select;
-use Atlas\Query\Update;
-use Atlas\Table\Row;
-use Atlas\Table\Table;
-use Atlas\Table\TableEvents;
-use PDOStatement;
-
-class NopkeyTableEvents extends TableEvents
+class NopkeyTableEvents extends _\NopkeyTableEvents_
 {
 }
